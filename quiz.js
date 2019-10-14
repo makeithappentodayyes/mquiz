bafef5b8c66bb8786b9052aa8fe9672a27d04b88
Daniel Gil <danielgilucsd@gmail.com>
2019-10-14 15:14:32 -0700

    changes made to questions

--- quiz.js
+++ quiz.js
@@ -1,45 +1,70 @@
-var questions = [
-    {
-        "title":" how many colums total?", 
-        "options": ["twelve","seven","three","four"],
-    "answer":"twelve"
-    },
+function submitAnswers() {
 
-    {
-        "title":" how many colums total?", 
-        "options": ["twelve","seven","three","four"],
-    "answer":"twelve"
-    },
+	var total = 4;
 
-   {
-        "title":" how many colums total?", 
-        "options": ["twelve","seven","three","four"],
-    "answer":"twelve"
-    },
-    {
-        "title":" how many colums total?", 
-        "options": ["twelve","seven","three","four"],
-    "answer":"twelve"
-    }
-]
+	var score = 0;
 
 
 
 
+	var q1 = document.forms["quizForm"]["q1"].value;
+
+	var q2 = document.forms["quizForm"]["q2"].value;
+
+	var q3 = document.forms["quizForm"]["q3"].value;
+
+	var q4 = document.forms["quizForm"]["q4"].value;
 
 
-// var questions = ("4")
 
-function gametime(){
+	
 
-  var questionelement = document.getElementById("question") 
-  var choices = document.querySelector("choice-button")
-console.log(questionelement)
+	
 
-  for (var i=0; i < questions.length; i++) {
-    questionelement.innerText = questions[i].title
-    choices.innerHTML = questions[i].options[0]
-    console.log(choices)
-}
+	for(i = 1; i <= total; i++) {
+
+		if(eval('q'+i) == null || eval('q'+i) == '') {
+
+			alert('You missed question '+ i);
+
+			return false;
+
+		}
+
+	}
+
+
+
+	
+
+	var answers = ["b", "b", "d", "c",];
+
+
+
+	
+
+	for(i = 1; i <= total; i++) {
+
+		if(eval('q'+1) == answers[i - 1]) {
+
+			score++;
+
+		}
+
+	}
+
+
+
+
+
+	var results = document.getElementById('results');
+
+	results.innerHTML = '<h3>Your score is <span>'+ score + '</span> out of <span>'+ total+'</span></h3>'
+
+	alert('Your score was ' + score + 'out of ' + total);
+
+
+
+	return false;
 
 }
